ad8dbeb96ffe6f27bc4285ba5e0b3e594e734f04
Wink Saville <wink@saville.com>
2020-09-09 09:29:27 -0700

    fix long-descriptoin_content_type maybe

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + "\n\n" + history,
-    long_description_content_type="x-rst",
+    long_description_content_type="text/x-rst",
     include_package_data=True,
     keywords="taperable_helix",
     name="taperable_helix",
