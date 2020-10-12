34751875e86b855e24ee869fa0fed7e0fe751248
Wink Saville <wink@saville.com>
2020-10-12 15:54:28 -0700

    bump version: 0.8.16 to 0.8.17

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/winksaville/py-taperable-helix",
-    version="0.8.16",
+    version="0.8.17",
     zip_safe=False,
 )
